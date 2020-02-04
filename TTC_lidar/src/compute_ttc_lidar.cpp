0625ceb1a0774b31fae21da5d66a3f97ac1a9450
Yousof <yousofebneddin@yahoo.com>
2020-02-03 23:46:00 -0800

    commit

--- TTC_lidar/src/compute_ttc_lidar.cpp
+++ TTC_lidar/src/compute_ttc_lidar.cpp
@@ -19,16 +19,25 @@ void computeTTCLidar(std::vector<LidarPoint> &lidarPointsPrev,
     double minXPrev = 1e9, minXCurr = 1e9;
     for (auto it = lidarPointsPrev.begin(); it != lidarPointsPrev.end(); ++it)
     {
-        minXPrev = minXPrev > it->x ? it->x : minXPrev;
+        
+        if (abs(it->y) <= laneWidth / 2.0)
+        { // 3D point within ego lane?
+            minXPrev = minXPrev > it->x ? it->x : minXPrev;
+        }
     }
 
     for (auto it = lidarPointsCurr.begin(); it != lidarPointsCurr.end(); ++it)
     {
-        minXCurr = minXCurr > it->x ? it->x : minXCurr;
+
+        if (abs(it->y) <= laneWidth / 2.0)
+        { // 3D point within ego lane?
+            minXCurr = minXCurr > it->x ? it->x : minXCurr;
+        }
     }
 
     // compute TTC from both measurements
     TTC = minXCurr * dT / (minXPrev - minXCurr);
+}
 }
 
 int main()
